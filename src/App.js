520e5cd06dd19ce96027b3fdfc39b5f5ba41a42f
YXR9 <yangxuru870918@gmail>
2021-07-18 16:58:29 +0800

    add delete function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 import Nav from './components/Nav.js'
 import Input from './components/Input.js'
@@ -10,40 +11,85 @@ class App extends React.Component {
     this.state={
       item:['讀書','寫作業','畫畫'],
       input:'',
+      completed:false,
+      id:nanoid(),
     };
-    this.Addtx=this.Addtx.bind(this);
+    this.addList=this.addList.bind(this);
   }
-  insert_text=(e)=>{
+  /* 於輸入框輸入清單項目 */
+  insertList=(e)=>{
     // console.log(e.target.value);
     if(e.target instanceof HTMLInputElement){
       this.setState({
         input:e.target.value,
+        completed:false,
+        id:nanoid(),
       })
     }
   }
-  Addtx=(a)=>{
-      const otheritem=[a.target.value,...this.state.item]
-      this.setState({
-        item:otheritem,
-        input:'',
-        check:true
-      })
-      // console.log('ok~');
-    
+
+  /* 按下Add按鈕，新增清單項目 */
+  addList=(a)=>{
+    const otheritem=[a.target.value,...this.state.item]
+    this.setState({
+      id:nanoid(),
+      item:otheritem,
+      input:'',
+      completed:false,
+    })
+    // console.log('ok~');
   }
+
+  /**按下清單項目的Delete按鈕，刪除清單項目 */
+  removeList=(index)=>{
+    const {item}=this.state;
+    item.splice(index,1);
+    this.setState({
+      item,
+    });
+  };
+
+  /**當勾選清單項目時的事件 */
+  toggleCompleted=(event,index)=>{
+    const listCompleted=this.state.item.filter((todo)=>{
+      if(todo.id===index+1){
+        todo.completed=event.target.checked;
+      }
+      return todo;
+    });
+    this.setState({
+      item: listCompleted,
+      completed:true,
+      id:nanoid(),
+    });
+  }
+
   //lifecycle
   componentDidUpdate() {
     console.log("Update")
   }
+
   render(){
     return (
       <div>
-        <h1>To Do List</h1>
-        <input type="text" value={this.state.input} onKeyPress={this.Addtx} onChange={this.insert_text}/>
-        <button onClick={this.Addtx} value={this.state.input}>Add</button>
-        <Nav />
-        <Input name={this.state.item}/>
-        
+        <h1 className="title">To Do List</h1>
+        <input 
+          className="input"
+          type="text" 
+          placeholder="Type item here..." 
+          onKeyPress={this.addList} 
+          onChange={this.insertList}
+        />
+        <button className="btn" onClick={this.addList} value={this.state.input}>Add</button>
+        <Nav name={this.state.item}/>
+        <Input
+          item={this.state.item}
+          value={this.state.input} 
+          id={this.state.id}
+          completed={this.state.completed}
+          handleOnChange={this.toggleCompleted}
+          handleOnRemove={this.removeList}
+        />
       </div>
     );
   }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,25 +1,127 @@
-import React from 'react';
+// import { useState } from 'react';
+
+// import { nanoid } from 'nanoid';
+// import './Input.css';
+
+// const arr = [];
+// function Data(props) {
+//     arr.push(props.id);
+//     console.log(arr);
+//     const deleteItem = (id) =>{
+//         return arr.filter(item => id !== item.id);
+//         // console.log(id);
+//         // console.log(newList);
+//         // this.useState({arr: newList});
+//     }
+//     return(
+//         <div className="inputData">
+//             <input type="checkbox" id="nanoid()" completed="false"/>
+//             <a className="inputdata">{props.name}</a>
+//             <button className="btn">Edit</button>
+//             <button className="btn" onClick={()=>deleteItem(props.id)}>Delete</button>
+//         </div>
+//     )
+
+// }
+
+// function Input(props){
+//     const list=props.name.map(value=>
+//         <Data id={nanoid()} key={value} name={value} completed={false}/>)
+//     // const [doList, setdoList] = useState(list);
+//     console.log(list);
+//     return(
+//         <div className="inputlist">
+//             <ul>{list}</ul>
+//         </div>
+//     )
+// }
+
+// export default Input;
+
+//-------------------------------------------------------------------------
+
+// import React, { useState } from 'react';
+// import { nanoid } from 'nanoid';
+// import './Input.css';
+// import { name } from 'commander';
+
+// function Input(props){
+//     const deleteItem=(id)=>{
+//         console.log(id);
+//         // alert("you success~");  //test1
+//         const list = props.name;
+//         // alert(list);    //test2
+//         // const newList = list.filter(item => item.id !== id);
+//     }
+//     return(
+//         props.name.map(value=>{
+//             return(
+//             <div className="inputlist">
+//                 <input type="checkbox" id="nanoid()"/>
+//                 <a className="inputdata" key="value" name="value" completed="false">{value}</a>
+//                 <button className="btn">Edit</button>
+//                 <button className="btn" onClick={()=>deleteItem(props.value.id)}>Delete</button>
+//             </div>
+//         )})
+//     )
+// }
+
+// export default Input;
+
+// import { useState } from 'react';
+// import { nanoid } from 'nanoid';
+// import './Input.css';
+// import deleteItem from '../App.js';
+
+
+// function Data(props) {
+//     return(
+//         <div className="inputData">
+//             <input type="checkbox" id="nanoid()" completed="false"/>
+//             <a className="inputdata">{props.name}</a>
+//             <button className="btn">Edit</button>
+//             <button className="btn" onClick={props.deleteItem}>Delete</button>
+//         </div>
+//     )
+// }
+
+// function Input(props){
+//     const list=props.name.map(value=>
+//         <Data id={nanoid()} key={value} name={value} completed={false}/>)
+//     // const [doList, setdoList] = useState(list);
+//     console.log(list);
+//     return(
+//         <div className="inputlist">
+//             <ul>{list}</ul>
+//         </div>
+//     )
+// }
+
+// export default Input;
+import React, { useState } from 'react';
 import './Input.css';
 
-function Data(props) {
-    return(
-        <div className="inputData">
-            <input type="checkbox" />
-            <a>{props.name}</a>
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>
-    )
-    
-}
-
 function Input(props){
-    const list=props.name.map(value=>
-        <Data key={value} name={value}/>)
-        console.log(list);
+    const {item,value,id,completed}=props;
+
+    const listItem=item.map((value,index,completed)=>{
+        return(
+            <div key={id}>
+                <div  className="inputData">
+                    <label>
+                        <input type="checkbox" onChange={(event)=>props.handleOnChange(event,index)}/>
+                    </label>
+                    <a className="inputdata">{value}</a>
+                    <button className="btn">Edit</button>
+                    <button className="btn" onClick={()=>props.handleOnRemove(index)}>Delete</button>
+                </div>
+            </div>
+        )
+    });
+
     return(
-        <div className="input">
-            <ul>{list}</ul>
+        <div className="inputlist">
+            {item.length>0 && listItem}
         </div>
     )
 }
